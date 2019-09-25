584c61c5a764c7ebfd937789ac581469a8c9d663
liam <lmshaw@calpoly.edu>
2019-09-25 09:21:32 -0700

    added comment

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-
+#this comment is just to test to see if connected to github
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
