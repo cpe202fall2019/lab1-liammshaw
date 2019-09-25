f6145fa6b223bd0b15285724a0e5d1c74cac4bd1
liam <lmshaw@calpoly.edu>
2019-09-25 11:02:59 -0700

    worked on a lot

--- lab1.py
+++ lab1.py
@@ -2,12 +2,24 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+      raise ValueError("List is None")
+   elif len(int_list)==0:
+         return None
+   else:
+      max_val = int_list[0]
+      for i in range(len(int_list)):
+         if int_list[i] > max_val:
+            max_val = int_list[i]
+      return max_val
+
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+      raise ValueError("List is None")
+   #elif len()
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,23 +1,43 @@
-aimport unittest
+import unittest
 from lab1 import *
 
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        """add description here"""
+        """tests if list is none"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
-    def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+    def test_max_list(self):
+        "tests to see if the general idea of the method works"
+        tlist = [1,2,3,5,6,9,8,7,6,18,23,6,78,9]
+        self.assertEqual(max_list_iter(tlist),78)
 
-    def test_bin_search(self):
-        list_val =[0,1,2,3,4,7,8,9,10]
-        low = 0
-        high = len(list_val)-1
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+    def test_list_empty(self):
+        "tests to see what happens if list is empty"
+        tlist =[]
+        self.assertEqual(max_list_iter(tlist),None)
+
+    def test_max_first(self):
+        "tests to see what happens when the largest number is first"
+        tlist =[201,9,8,7,6,5,4,3,2,1]
+        self.assertEqual(max_list_iter(tlist),201)
+
+    def test_max_last(self):
+        "test to see what happens when the largest number is last"
+        tlist = [1,2,3,4,5,6,7,8,9,10,11,12,301]
+        self.assertEqual(max_list_iter(tlist),301)
+
+    #def test_reverse_rec(self):
+        #self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+
+    #def test_bin_search(self):
+        #list_val =[0,1,2,3,4,7,8,9,10]
+        #low = 0
+        #high = len(list_val)-1
+        #self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
 
 if __name__ == "__main__":
         unittest.main()
